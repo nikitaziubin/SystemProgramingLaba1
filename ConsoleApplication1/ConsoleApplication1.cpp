2ed4dfa75eca46e69588964cd8528dff831d8b28
nikita.ziubin <nikita.ziubin@gmail.com>
2023-09-11 23:09:34 +0300

    Task14 find biggest M (not final code)

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -21,7 +21,6 @@ int main()
     if (a)
     {
         getline(in, line);
-        //cout << line << endl;
     }
     in.close();
 
@@ -124,16 +123,38 @@ int main()
         size++;
     }
     cout << endl;
-
+    
+    string lines14[1000][2];
+    int oneLineLength = linesFinal[0].length();
+    int beigestM = 0;
+    int midM = -1;
+    for (int i = 0; i < size; i++)
+    {
+        for (int k = 0; k < oneLineLength; k++)
+        {
+            char a = linesFinal[i][k];
+            char b = linesFinal[i + 1][k];
+            if (a != b)
+            {
+                beigestM++;
+            }
+        }
+        if (beigestM >= midM)
+        {
+            if (beigestM == midM)
+            {
+                lines14[i][0] = linesFinal[i];
+                lines14[i][1] = linesFinal[i + 1];
+            }
+            else
+            {
+                /*midM = beigestM;
+                beigestM = 0;
+                fill(linesFinal, linesFinal + size, "");
+                lines14[i][0] = linesFinal[i];
+                lines14[i][1] = linesFinal[i + 1];*/
+            }
+        }
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
